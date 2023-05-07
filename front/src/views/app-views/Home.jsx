de38ccf52d8be59b7d804f7d5f526e5902516389
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-07 18:03:49 -0300

    main ok

--- front/src/views/app-views/Home.jsx
+++ front/src/views/app-views/Home.jsx
@@ -4,7 +4,7 @@ import React from "react";
     VIEW Home - "/" - Vista principal de la página
 
     styles:
-    carrousel con imagenes de fiestas 
+    carrousel con imagenes de fiestas
     sección eventos destacados: filtros (por provincia, por productora) + orden (por fecha) + lista de eventos destacados 
     info de la pagina c link a about
     preguntas frecuentes
