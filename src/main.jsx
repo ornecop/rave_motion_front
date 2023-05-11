969e001c3030142b2c529f23fb6761c7b6d96e4b
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-11 13:05:52 -0300

    redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -10,15 +10,21 @@ import "./index.css";
 // React router dom
 import { BrowserRouter } from "react-router-dom";
 
+// ======================== React Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 // Google OAuth
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <GoogleOAuthProvider clientId="304343013242-6qim8t96pm35vpum87e0saa58sfomrt3.apps.googleusercontent.com">
-        <React.StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </React.StrictMode>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+        <GoogleOAuthProvider clientId="304343013242-6qim8t96pm35vpum87e0saa58sfomrt3.apps.googleusercontent.com">
+            <React.StrictMode>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </React.StrictMode>
+        </GoogleOAuthProvider>
+    </Provider>
 );
