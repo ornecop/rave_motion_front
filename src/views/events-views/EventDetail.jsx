dc22e69817fefc41feecb07d221924e88a58ace9
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-15 08:41:19 -0300

    detail improve

--- src/views/events-views/EventDetail.jsx
+++ src/views/events-views/EventDetail.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect } from "react";
+/* =======================================================
+    VIEW EventDetail - "/event/:eventName" - Vista a la que redirección al tocar un evento
+*/
+
+// Redux
 import { useDispatch, useSelector } from "react-redux";
-import { MdLocationOn, MdDateRange, MdWatch } from "react-icons/md";
-import { Link, useParams } from "react-router-dom";
 import {
     getEventById,
     removeEventDetail,
 } from "../../redux/actions/eventsActions";
-/* =======================================================
-    VIEW EventDetail - "/event/:eventName" - Vista a la que redirección al tocar un evento
-*/
+
+// Assets
+import { AiOutlineCalendar } from "react-icons/ai";
+import { ImLocation2 } from "react-icons/im";
+
+// React router dom
+import { Link, useParams } from "react-router-dom";
 
 const EventDetail = () => {
     const { id } = useParams();
@@ -23,78 +30,102 @@ const EventDetail = () => {
         };
     }, []);
 
+    // Formateo de fecha y hour
+    const date = new Date(event.date);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString();
+
+    const formatDate = `${day}-${month}-${year}`;
+
+    console.log(event.hour);
+    const formatHour = "dsds!";
+
     return (
-        <div className="floatBox max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4 font-sans bg-secondary">
-            {/* Detalles del evento */}
-            <div className="p-4 bg-secondary text-center">
-                {event.image && (
-                    <img
-                        className="w-full rounded"
-                        src={event.image}
-                        alt={event.name}
-                    />
-                )}
-                <div className="p-4">
-                    <div className="uppercase tracking-wide text-3xl font-semibold text-white">
-                        {event.name}
-                    </div>
-                    <p className="mt-2 text-white flex justify-center items-center">
-                        <MdLocationOn /> Venue: {event.venue}
-                    </p>
-                    <p className="mt-2 text-white flex justify-center items-center">
-                        {" "}
-                        Producer: {event.producer}
-                    </p>
-                    <p className="mt-2 text-white flex justify-center items-center">
-                        <MdDateRange /> Date: {event.date}
-                    </p>
-                    <p className="mt-2 text-white flex justify-center items-center">
-                        <MdWatch /> Hour: {event.hour}
-                    </p>
-                    <div className="mt-4 p-2 rounded">
-                        <p className="text-white text-lg leading-relaxed">
-                            {event.description}
-                        </p>
-                    </div>
-                </div>
+        <div className="w-screen">
+            <div className="h-96 relative overflow-hidden">
+                <div
+                    className="h-full w-full absolute top-0 left-0 bg-center bg-repeat-x "
+                    style={{
+                        backgroundImage: `url(${event.image})`,
+                    }}
+                ></div>
             </div>
 
-            {/* Lista de tickets */}
-            <div className="mt-4 bg-secondary">
-                <div className="px-4 pt-3 pb-2 text-white">Tickets</div>
-                {event.Tickets &&
-                    event.Tickets.map((ticket, index) => (
+            <div className="floatBox max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4 font-sans bg-secondary">
+                <div className="h-32 flex items-center justify-center ">
+                    <h2 className="text-4xl text-center align-center font-semibold">
+                        {event.name}
+                    </h2>
+                </div>
+                {/* Detalles del evento */}
+                <div className=" flex flex-col text-center">
+                    <div className="h-96 w-96 rounded-xl border border-secondaryBorder self-center">
                         <div
-                            key={index}
-                            className="p-4 mt-4 bg-primary rounded-xl"
-                        >
-                            <div className="px-4 py-3">
-                                <div className="text-sm font-semibold text-white">
-                                    Name: {ticket.name}
-                                </div>
-                                <p className="mt-1 text-sm text-white">
-                                    Description: {ticket.description}
-                                </p>
-                                <p className="mt-1 text-sm text-white">
-                                    Access Type: {ticket.accessType}
-                                </p>
-                                <p className="mt-1 text-sm text-white">
-                                    Price: {ticket.price}
-                                </p>
-                                <p className="mt-1 text-sm text-white">
-                                    Max Quantity: {ticket.maxQuantity}
-                                </p>
-                                <p className="mt-1 text-sm text-white">
-                                    Description: {ticket.description}
-                                </p>
-                                <Link to="/cart">
-                                    <button className="mt-3 bg-white hover:bg-gray-200 text-primary font-bold py-2 px-4 rounded">
-                                        Comprar
-                                    </button>
-                                </Link>
-                            </div>
+                            className="h-full w-full bg-cover bg-bottom bg-no-repeat place-content-center rounded-xl"
+                            style={{
+                                backgroundImage: `url(${event.image})`,
+                            }}
+                        ></div>
+                    </div>
+                    <div className="p-4">
+                        <div className="flex flex-row justify-center items-center gap-2 mt-4">
+                            <ImLocation2 size="1.3rem" />
+                            <span>
+                                <span className="font-semibold">
+                                    {event.producer}
+                                </span>{" "}
+                                - {event.venue}
+                            </span>
+                            <div className="w-4 font-semibold"></div>
+                            <AiOutlineCalendar size="1.3rem" />
+                            <span className="">
+                                {formatDate} - {formatHour}
+                            </span>
                         </div>
-                    ))}
+                        <div className="flex flex-row justify-center items-center gap-2 mt-4 w-full min-h-fit overflow-y-scroll">
+                            {event.description}
+                        </div>
+                    </div>
+                </div>
+
+                {/* Lista de tickets */}
+                <div className="mt-4 bg-secondary">
+                    <div className="px-4 pt-3 pb-2 text-white">Tickets</div>
+                    {event.Tickets &&
+                        event.Tickets.map((ticket, index) => (
+                            <div
+                                key={index}
+                                className="p-4 mt-4 bg-primary rounded-xl"
+                            >
+                                <div className="px-4 py-3">
+                                    <div className="text-sm font-semibold text-white">
+                                        Name: {ticket.name}
+                                    </div>
+                                    <p className="mt-1 text-sm text-white">
+                                        Description: {ticket.description}
+                                    </p>
+                                    <p className="mt-1 text-sm text-white">
+                                        Access Type: {ticket.accessType}
+                                    </p>
+                                    <p className="mt-1 text-sm text-white">
+                                        Price: {ticket.price}
+                                    </p>
+                                    <p className="mt-1 text-sm text-white">
+                                        {ticket.maxQuantity} / {ticket.sells}
+                                    </p>
+                                    <p className="mt-1 text-sm text-white">
+                                        Description: {ticket.description}
+                                    </p>
+                                    <Link to="/cart">
+                                        <button className="mt-3 bg-white hover:bg-gray-200 text-primary font-bold py-2 px-4 rounded">
+                                            Comprar
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                </div>
             </div>
         </div>
     );
