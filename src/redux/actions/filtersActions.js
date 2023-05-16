01730b233e7e87575cd91e723f7af0dcb510b6c4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-05-16 09:29:07 -0300

    filtros fechas funcionando

--- src/redux/actions/filtersActions.js
+++ src/redux/actions/filtersActions.js
@@ -5,17 +5,21 @@ export const EVENTS_FILTER = "EVENTS_FILTER";
 
 
 export const filteredEvents = ({startDate,endDate,producer}) =>{
-    console.log(startDate,endDate,producer)
+
     return async(dispatch)=>{
         let filteredEvents;
 
-        if(producer ===null && startDate===null && endDate===null) { const response = await axios.get(`http://localhost:3001/events/filter`);
+            if(producer ===null && startDate && endDate===null) { const response = await axios.get(`http://localhost:3001/events/filter?startDate=${startDate}`);
             filteredEvents = response.data;}    
-        
+
             else if(startDate && endDate && producer){
             const response = await axios.get(`http://localhost:3001/events/filter?producer=${producer}&startDate=${startDate}&endDate=${endDate}`);
             filteredEvents=response.data;}
-        
+            
+            else if(startDate && endDate === null && producer){
+            const response = await axios.get(`http://localhost:3001/events/filter?producer=${producer}&startDate=${startDate}`);
+            filteredEvents=response.data;}
+
             else if(producer === null){
             const response = await axios.get(`http://localhost:3001/events/filter?startDate=${startDate}&endDate=${endDate}`);
             filteredEvents =response.data;}
