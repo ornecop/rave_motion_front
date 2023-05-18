6b39ed0be389adab1a2558a4693ef4236e70603b
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-18 10:07:38 -0300

    styles

--- src/views/events-views/EventTicketsCreate.jsx
+++ src/views/events-views/EventTicketsCreate.jsx
@@ -198,13 +198,6 @@ const EventTicketsCreate = (props) => {
                                 </div>
 
                                 {/* Divider */}
-                                <div>
-                                    <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-                                        <p className="mx-4 mb-0 text-center font-semibold">
-                                            Crear o modificar tickets:
-                                        </p>
-                                    </div>
-                                </div>
 
                                 {/* name */}
                                 <div className="flex flex-col my-2">
