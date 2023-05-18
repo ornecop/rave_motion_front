c5635fcca8b6035e4c29f4fac5d76a3816dfc0b4
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-18 20:38:17 -0300

    event create add eventId param

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ const App = ({ verifyToken, isLogin, userData }) => {
                 <Route path="/event/:id" element={<EventDetail />} />
                 {/* Secure Routes */}
                 <Route
-                    path="/create"
+                    path="/create/:eventId"
                     element={
                         <RequireAuth>
                             <EventCreate />
