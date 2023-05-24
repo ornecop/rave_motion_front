db2bfc7f81df5606eec44561d980a4f07e1f7b6b
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-23 21:31:56 -0300

    set & remove global error /success

--- src/redux/actions/appActions.js
+++ src/redux/actions/appActions.js
@@ -1,10 +1,34 @@
 // ============= Events Actions Types
+export const GLOBAL_ERROR_SET = "GLOBAL_ERROR_SET";
 export const GLOBAL_ERROR_REMOVE = "GLOBAL_ERROR_REMOVE";
 
+export const GLOBAL_SUCCESS_SET = "GLOBAL_SUCCESS_SET";
+export const GLOBAL_SUCCESS_REMOVE = "GLOBAL_SUCCESS_REMOVE";
+
 // ============= Events Actions Creators
+
+export const setGlobalError = (error) => {
+    return {
+        type: GLOBAL_ERROR_SET,
+        payload: error,
+    };
+};
 
 export const removeGlobalError = () => {
     return {
         type: GLOBAL_ERROR_REMOVE,
+    };
+};
+
+export const setGlobalSuccess = (message) => {
+    return {
+        type: GLOBAL_SUCCESS_SET,
+        payload: message,
+    };
+};
+
+export const removeGlobalSuccess = () => {
+    return {
+        type: GLOBAL_SUCCESS_REMOVE,
     };
 };
