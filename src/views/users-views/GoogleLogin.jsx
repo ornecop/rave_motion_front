502441c0df90e782359f5c7d6162a9c6592a8f02
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-24 13:41:25 -0300

    ok2

--- src/views/users-views/GoogleLogin.jsx
+++ src/views/users-views/GoogleLogin.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect } from "react";
-import jwt_decode from "jwt-decode" 
-import { signInGoogle } from "../../redux/actions/usersActions";
+import { useEffect } from "react";
+
+// JWT
+import jwt_decode from "jwt-decode";
+
+// Redux
 import { useDispatch } from "react-redux";
-function GoogleAuthComponent() {
-const dispatch= useDispatch()
-  function handleCallbackResponse (response){
-    const userObject= jwt_decode(response.credential)
-  
-    const { given_name: name, family_name: lastname, email: mail } = userObject;
-    const extractedData = { name, lastname, mail };
-    dispatch(signInGoogle(extractedData))
-      }
- 
-  useEffect(() => {
-google.accounts.id.initialize({
-  client_id:"255049858573-qm7dl82tt6j3bj837067gb83snm2ihup.apps.googleusercontent.com",
-  callback: handleCallbackResponse
-})
-google.accounts.id.renderButton(
-  document.getElementById("signInDiv"),
-  {theme:"outline",size:"large"},
-)
-  }, []);
+import { signInGoogle } from "../../redux/actions/usersActions";
 
-  return (
-    <div>
+const GoogleAuthComponent = () => {
+    const dispatch = useDispatch();
+    function handleCallbackResponse(response) {
+        const userObject = jwt_decode(response.credential);
 
-        <div id="signInDiv"></div>
-      
-    </div>
-)}
+        const {
+            given_name: name,
+            family_name: lastname,
+            email: mail,
+        } = userObject;
+        const extractedData = { name, lastname, mail };
+        dispatch(signInGoogle(extractedData));
+    }
+
+    useEffect(() => {
+        google.accounts.id.initialize({
+            client_id:
+                "255049858573-qm7dl82tt6j3bj837067gb83snm2ihup.apps.googleusercontent.com",
+            callback: handleCallbackResponse,
+        });
+        google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+            theme: "outline",
+            size: "large",
+        });
+    }, []);
+
+    return (
+        <div>
+            <div id="signInDiv"></div>
+        </div>
+    );
+};
 
 export default GoogleAuthComponent;
