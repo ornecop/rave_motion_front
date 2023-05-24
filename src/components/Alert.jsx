9ac83b27d19f408e9363cec0c651dcdf4af9f547
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-24 13:09:24 -0300

    stylos personalizados por location

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,5 +1,6 @@
 // Hooks
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 // Redux
 import { connect } from "react-redux";
@@ -19,8 +20,24 @@ const Alert = ({ globalError, removeGlobalError }) => {
         removeGlobalError();
     };
 
+    // Styles por ruta
+    const location = useLocation().pathname;
+
+    const [ubicStyle, setUbicStyle] = useState("hidden");
+
+    const style1 = "flex justify-center absolute z-10 w-screen mt-20";
+    const style2 = "flex justify-center absolute z-10 w-screen mt-48";
+    const style3 = "flex justify-center w-screen pt-20";
+    useEffect(() => {
+        if (location.slice(0, 7) === "/create") setUbicStyle(style1);
+
+        if (location === "/") setUbicStyle(style2);
+
+        if (location.slice(0, 6) === "/event") setUbicStyle(style3);
+    }, [location]);
+
     return (
-        <div className="flex justify-center absolute z-10 w-screen mt-20">
+        <div className="ubicStyle">
             <div className="relative py-5 px-16 floatBox border-red-400 text-red-400">
                 <button
                     type="button"
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,5 +1,6 @@
 // Hooks
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 // Redux
 import { connect } from "react-redux";
@@ -20,8 +21,24 @@ const Success = ({ globalSuccess, removeGlobalSuccess }) => {
         removeGlobalSuccess();
     };
 
+    // Styles por ruta
+    const location = useLocation().pathname;
+
+    const [ubicStyle, setUbicStyle] = useState("hidden");
+
+    const style1 = "flex justify-center absolute z-10 w-screen mt-20";
+    const style2 = "flex justify-center absolute z-10 w-screen mt-48";
+    const style3 = "flex justify-center w-screen pt-20";
+    useEffect(() => {
+        if (location.slice(0, 7) === "/create") setUbicStyle(style1);
+
+        if (location === "/") setUbicStyle(style2);
+
+        if (location.slice(0, 6) === "/event") setUbicStyle(style3);
+    }, [location]);
+
     return (
-        <div className="flex justify-center absolute z-10 w-screen mt-20">
+        <div className={ubicStyle}>
             <div className="relative py-5 px-16 floatBox border-green-400 text-green-400">
                 <button
                     type="button"
