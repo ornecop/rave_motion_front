228fb399b24762aa32818bee462a1ac2ef8fcc36
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-24 13:49:54 -0300

    fix

--- src/views/app-views/About.jsx
+++ src/views/app-views/About.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { useState } from "react";
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
-
 /* =======================================================
-    VIEW About - "/about" - 
+VIEW About - "/about" - 
 
-    styles:
-    
-
+styles:
 */
-import Paginado from '../../components/Paginado'
+// Hooks
+import { useState } from "react";
+
+// Assets
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+// Paginado va¿?
+import Paginado from "../../components/Paginado";
+
 const About = () => {
-    
     const allEventos = [
         {
             id: 1,
@@ -76,66 +77,71 @@ const About = () => {
             github: "https://github.com/juanperez",
             linkedin: "https://www.linkedin.com/in/juanperez/",
         },
-        
     ];
     // const [order, setOrder] = useState(1);
-    const [currentPage, setCurrentPage] = useState(1)
-    const [eventsPerPage] = useState(3)
-    const indexOfLastEvent = currentPage * eventsPerPage
-    const indexOfFirstEvent = indexOfLastEvent - eventsPerPage
-    const currentEvents = allEventos.slice(indexOfFirstEvent, indexOfLastEvent)
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const [eventsPerPage] = useState(3);
+    const indexOfLastEvent = currentPage * eventsPerPage;
+    const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+    const currentEvents = allEventos.slice(indexOfFirstEvent, indexOfLastEvent);
 
     const paginado = (pageNumber) => {
-        setCurrentPage(pageNumber)
+        setCurrentPage(pageNumber);
     };
 
     return (
         <div className="w-full min-h-screen">
             <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-16">
-                {currentEvents.map((compañero) => (
-                    <div
-                        className="m-8 w-4/5 bg-secondary border border-secondaryBorder shadow-md rounded-2xl "
-                        key={compañero.id}>
-                        <img
-                            className="w-5/6 m-8 rounded-full mx-auto transition-all duration-300 hover:rounded-lg"
-                            src={compañero.foto}
-                            alt={compañero.nombre}/>
-                        <h3 className="text-2xl uppercase text-white font-semibold text-center mb-4">
-                            {compañero.nombre}
-                        </h3>
-                        <p className="text-gray-200 text-center ">
-                            {compañero.correo}</p>
-                        <p className="text-gray-200 text-center mb-6 ">
-                            {compañero.telefono}
-                        </p>
-                        
-                    <div className="flex justify-center mb-8">
-                        <p className="flex items-center mx-6">
-                        <a className="inline-flex items-center text-white hover:text-blue-600"
-                        href={compañero.github}>
-                        <FaGithub className="mr-2 text-3xl" /> 
-                        </a>
-                        </p>
-                        <p className="flex items-center mx-6">
-                        <a className="inline-flex items-center text-white hover:text-blue-600"
-                        href={compañero.linkedin}>
-                        <FaLinkedin className="mr-2 text-3xl" /> 
-                        </a>
-                        </p>
-                    </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-16">
+                    {currentEvents.map((compañero) => (
+                        <div
+                            className="m-8 w-4/5 bg-secondary border border-secondaryBorder shadow-md rounded-2xl "
+                            key={compañero.id}
+                        >
+                            <img
+                                className="w-5/6 m-8 rounded-full mx-auto transition-all duration-300 hover:rounded-lg"
+                                src={compañero.foto}
+                                alt={compañero.nombre}
+                            />
+                            <h3 className="text-2xl uppercase text-white font-semibold text-center mb-4">
+                                {compañero.nombre}
+                            </h3>
+                            <p className="text-gray-200 text-center ">
+                                {compañero.correo}
+                            </p>
+                            <p className="text-gray-200 text-center mb-6 ">
+                                {compañero.telefono}
+                            </p>
 
+                            <div className="flex justify-center mb-8">
+                                <p className="flex items-center mx-6">
+                                    <a
+                                        className="inline-flex items-center text-white hover:text-blue-600"
+                                        href={compañero.github}
+                                    >
+                                        <FaGithub className="mr-2 text-3xl" />
+                                    </a>
+                                </p>
+                                <p className="flex items-center mx-6">
+                                    <a
+                                        className="inline-flex items-center text-white hover:text-blue-600"
+                                        href={compañero.linkedin}
+                                    >
+                                        <FaLinkedin className="mr-2 text-3xl" />
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-                   
-
-            </div> 
             </div>
-       
-            <Paginado eventsPerPage={eventsPerPage} allEventos={allEventos.length} paginado={paginado}/>
+
+            <Paginado
+                eventsPerPage={eventsPerPage}
+                allEventos={allEventos.length}
+                paginado={paginado}
+            />
         </div>
-        
     );
 };
 
