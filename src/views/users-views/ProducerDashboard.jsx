2ac3027e65fd5d103bac431feffee37c838bdd41
EnmaReyes <enmareco.co@gmail.com>
2023-05-25 10:07:13 -0400

    #

--- src/views/users-views/ProducerDashboard.jsx
+++ src/views/users-views/ProducerDashboard.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { signout } from "../../redux/actions/usersActions";
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'; 
-
+import { HiUser, HiUserCircle, HiKey, HiArrowLeftCircle, HiTableCells, HiSquaresPlus,} from "react-icons/hi2";
+import { BiEditAlt, BiTrash} from "react-icons/bi";
 
       //? importar view \\
 import DashboardCard from './DashboardCard'
@@ -84,28 +85,23 @@ const ProducerDashboard = () => {
        <ul className="space-y-2 font-medium mt-6 ">
 
        <li>
-            <Link className={`flex items-center p-2 text-white rounded-lg  hover:bg-secondary hover:text-xl transition duration-1000 ease-out ${open == true? "bg-secondary": ""}`} >
-               <svg className="h-6 w-6 text-white "  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-               <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="7" r="4" />  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
-               <span className="flex-1 ml-3 whitespace-nowrap" onClick={()=> setOpen(!open)}>Usuario </span>
+            <Link className={`flex items-center p-2 text-white rounded-lg  hover:bg-secondary hover:text-xl transition duration-1000 ease-out ${open == true? "bg-secondary": ""}`} > 
+            <HiUser className='h-6 w-6 inline'/> 
+            <span className="flex-1 ml-3 whitespace-nowrap" onClick={()=> setOpen(!open)}>Usuario </span>
             </Link>
             <div className={`${!open && "hidden"} `}>
                <div >
 
                <div className={`ml-14 mb-1 mt-1 p-2 text-sm hover:text-base rounded-lg  hover:bg-secondary transition duration-1000 ease-out ${selectedView == "cart"? "bg-secondary": ""}`}>
                      <Link onClick={() => handleViewClick('perfil')}>
-                     <svg class="h-6 w-6 text-white inline"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                     </svg>
+                     <HiUserCircle className='h-6 w-6 inline'/>
                      <span className="flex-1 ml-3 whitespace-nowrap ">Perfil</span>
                      </Link> 
                   </div>
 
                <div className= {`ml-14 mb-5 p-2 text-sm hover:text-base rounded-lg transition duration-1000 ease-out  hover:bg-secondary ${selectedView == "changepassword"? "bg-secondary": ""}`} >
                      <Link onClick={() => handleViewClick('changepassword')} className={` `}>
-                     <svg className="h-6 w-6 text-white inline" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeLinejoin="round"  >
-                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"/>
-                     </svg>
+                     <HiKey className='h-6 w-6 inline' />
                      <span className="flex-1 ml-3 whitespace-nowrap">Contraseña</span>
                      </Link>
                </div>
@@ -117,7 +113,7 @@ const ProducerDashboard = () => {
 
          <li>
             <Link className={`flex items-center p-2 text-white rounded-lg hover:bg-secondary hover:text-xl transition duration-1000 ease-out ${selectedView == "DashboardCard"? "bg-secondary": ""}`} onClick={() => handleViewClick('DashboardCard')}>
-               <svg aria-hidden="true" className="w-6 h-6  text-white transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+              <HiTableCells className='h-6 w-6 inline' />
                <span className="ml-3">Dashboard</span>
             </Link>
          </li>
@@ -125,8 +121,7 @@ const ProducerDashboard = () => {
          <li>
             <Link href="#" className={`flex items-center p-2 rounded-lg dark:text-white hover:bg-secondary  hover:text-xl transition duration-1000 ease-out ${selectedView == "ProducerEventDetail"? "bg-secondary": ""}`} 
             onClick={() => handleViewClick('ProducerEventDetail')}>
-
-               <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-white transition duration-75  group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
+            <HiSquaresPlus className='h-6 w-6 inline' />
                <span className="flex-1 ml-3 whitespace-nowrap text-whit ">Detalle Evento</span>
             </Link>
          </li>
@@ -134,26 +129,21 @@ const ProducerDashboard = () => {
          <li>
             <Link className={`flex items-center p-2 text-white rounded-lg hover:bg-secondary hover:text-xl transition duration-1000 ease-out ${selectedView == "EventTicketsCreate"? "bg-secondary": ""}`}
                onClick={() => handleViewClick('EventTicketsCreate')}>
-
-               <svg className="h-6 w-6 text-white "  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" />  <line x1="14.5" y1="5.5" x2="18.5" y2="9.5" />  <polyline points="12 8 7 3 3 7 8 12" />  <line x1="7" y1="8" x2="5.5" y2="9.5" />  <polyline points="16 12 21 17 17 21 12 16" />  <line x1="16" y1="17" x2="14.5" y2="18.5" /></svg>
+               <BiEditAlt className='h-6 w-6 inline' />
                <span className="flex-1 ml-3 whitespace-nowrap">Editar Evento</span>
             </Link>
          </li>
          <li>
             <Link className={`flex items-center p-2 text-white rounded-lg hover:bg-secondary hover:text-xl transition duration-1000 ease-out ${selectedView == "deleteEvent"? "bg-secondary": ""}`}
              onClick={() => handleViewClick('deleteEvent')}>
-            <svg className="h-6 w-6 text-white"  width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-            </svg>
+          <BiTrash className='h-6 w-6 inline'/>
                <span className="flex-1 ml-3 whitespace-nowrap">Eliminar Evento</span>
             </Link>
          </li>
       
          <li>
             <Link to={"/"} className={`flex items-center p-2 text-white rounded-lg  hover:bg-secondary hover:text-xl `}>
-               <svg className="h-6 w-6 text-white "  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"/>
-               </svg>
+               <HiArrowLeftCircle className='h-6 w-6 inline' />
                <botton className="flex-1 ml-3 whitespace-nowrap" onClick={handleSignOut} >Cerrar sesión</botton>
             </Link>
          </li>
