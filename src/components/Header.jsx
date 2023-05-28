3a7c9c72071a6f5ac78b04662b23fca4f08578c2
ornecop <orne.el38@gmail.com>
2023-05-28 14:13:47 -0300

    autocomp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ import { useState, useEffect, useRef } from "react";
 // React Router Dom
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
+
+
+
+
 // Redux
 import { connect } from "react-redux";
 import { getEventsByName } from "../redux/actions/eventsActions";
@@ -11,78 +15,102 @@ import { signOut } from "../redux/actions/usersActions";
 
 // Assets
 import rave from "../assets/logo3.png";
+import SearchResults from "../views/app-views/SearchResults";
 
 const Header = (props) => {
-    const { isLogin, userData, signOut } = props;
-    const { getEventsByName } = props;
+  const { isLogin, userData, signOut } = props;
+  const { getEventsByName } = props;
 
-    // Fondo opaco
-    const [opacity, setOpacity] = useState(0);
-    const handleScroll = () => {
-        if (window.pageYOffset > window.innerHeight * 0.4) {
-            setOpacity(0.7);
-        } else {
-            setOpacity(window.pageYOffset / (window.innerHeight * 0.4) / 1.42);
-        }
-    };
+  // Fondo opaco
+  const [opacity, setOpacity] = useState(0);
+  const handleScroll = () => {
+    if (window.pageYOffset > window.innerHeight * 0.4) {
+      setOpacity(0.7);
+    } else {
+      setOpacity(window.pageYOffset / (window.innerHeight * 0.4) / 1.42);
+    }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    const headerStyle = {
-        backgroundColor: `rgba(2, 6, 23, ${opacity})`,
-    };
+  const headerStyle = {
+    backgroundColor: `rgba(2, 6, 23, ${opacity})`,
+  };
 
-    // Dropdown
-    const [showDropdown, setShowDropdown] = useState(false);
+  // Dropdown
+  const [showDropdown, setShowDropdown] = useState(false);
     const dropdownRef = useRef(null);
-
-    const handlDropdownClick = () => {
-        setShowDropdown((prev) => !prev);
+    let hideTimeout; 
+    
+    const handleOptionClick = () => {
+      setShowDropdown(false);
     };
-
-    const location = useLocation().pathname;
-    useEffect(() => {
-        showDropdown && setShowDropdown(false);
-    }, [location]);
-
+    
     const handleOutsideClick = (event) => {
-        if (
-            dropdownRef.current &&
-            !dropdownRef.current.contains(event.target)
-        ) {
-            setShowDropdown(false);
-        }
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        !event.target.classList.contains("navLinkDropdown")
+      ) {
+        setShowDropdown(false);
+      }
     };
-
+    
     useEffect(() => {
-        document.addEventListener("mousedown", handleOutsideClick);
-        return () => {
-            document.removeEventListener("mousedown", handleOutsideClick);
-        };
+      document.addEventListener('mousedown', handleOutsideClick);
+      return () => {
+        document.removeEventListener('mousedown', handleOutsideClick);
+      };
     }, []);
-
-    // Search
-    const [name, setName] = useState("");
-    const handleInputChange = (event) => {
-        setName(event.target.value);
+    
+    const handleMouseEnter = () => {
+      clearTimeout(hideTimeout); 
+      setShowDropdown(true);
+    };
+    
+    const handleMouseLeave = () => {
+      hideTimeout = setTimeout(() => {
+        setShowDropdown(false);
+      }, 100); 
     };
 
-    const navigate = useNavigate();
-    const handleSearchsubmit = (event) => {
-        event.preventDefault();
-        navigate("/search");
-        getEventsByName(name.trim());
-        setName("");
-    };
+  // Search
+  const [name, setName] = useState("");
+  const [searchResults, setSearchResults] = useState([]); // Estado para almacenar los resultados de búsqueda
 
-    // Sign Out
-    const handleSignOut = () => {
-        isLogin && signOut();
-        navigate("/");
-    };
+  const handleInputChange = (event) => {
+    const searchValue = event.target.value;
+    setName(searchValue);
+
+    if (searchValue.trim() !== "") {
+      getEventsByName(searchValue.trim())
+        .then((response) => {
+          setSearchResults(response); 
+        })
+        .catch((error) => {
+        });
+    } else {
+      setSearchResults([]); 
+    }
+  };
+
+  const navigate = useNavigate();
+  const handleSearchsubmit = (event) => {
+    event.preventDefault();
+    navigate("/search");
+    getEventsByName(name.trim());
+    setName("");
+    
+  };
+
+  // Sign Out
+  const handleSignOut = () => {
+    isLogin && signOut();
+    navigate("/");
+  };
 
     return (
         <div
@@ -95,16 +123,24 @@ const Header = (props) => {
                 </Link>
             </div>
             <div className="flex justify-self-center items-center">
-                <form onSubmit={handleSearchsubmit}>
-                    <input
-                        className="w-96 input"
-                        type="text"
-                        placeholder="Buscar evento"
-                        onChange={handleInputChange}
-                        onSubmit={handleSearchsubmit}
-                        value={name}
-                    />
-                </form>
+   
+            <form onSubmit={handleSearchsubmit}>
+          <input
+            className="w-96 input"
+            type="text"
+            placeholder="Buscar evento"
+            onChange={handleInputChange}
+            onSubmit={handleSearchsubmit}
+            value={name}
+          />
+          {searchResults && searchResults.length > 0 && ( // Mostrar los resultados de búsqueda si existen
+            <ul>
+              {searchResults.map((allEvents) => (
+                <li key={allEvents.id}>{allEvents.name}</li>
+              ))}
+            </ul>
+          )}
+        </form>
             </div>
             <div className="flex w-fit justify-self-end justify-center my-2 items-center gap-6 py-2 px-4 bg-secondary rounded-full border border-secondaryBorder">
                 {/* <Link to="/" className="navLink">
@@ -119,29 +155,28 @@ const Header = (props) => {
                 </Link>
                 {isLogin ? (
                     <>
-                        {/* Dropdown user  */}
-                        <div
-                            className="inline-block relative"
-                            ref={dropdownRef}
-                        >
+                          {/* Dropdown user  */}
+                          <div className="inline-block relative" ref={dropdownRef}>
                             <button
-                                onClick={handlDropdownClick}
-                                className="btnPrimary py-0 px-4 w-fit border-none"
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            className="btnPrimary py-0 px-4 w-fit border-none"
                             >
-                                Tu cuenta
-                            </button>
-                            <div
-                                className={`"z-20 bg-secondary rounded-md w-40 left-[-2rem] top-[2rem] text-center" ${
-                                    showDropdown ? "block" : "hidden"
-                                }`}
-                                style={{ position: "absolute" }}
-                            >
-                                <div className="dropDownItem border-b-2 border-secondaryBorder">
-                                    <Link
-                                        className="navLinkDropdown"
-                                        to="/tickets"
-                                    >
-                                        Mis tickets
+                            Tu cuenta
+                             </button>
+                        <div
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            className={`"z-20 bg-secondary rounded-md w-40 left-[-2rem] top-[2rem] text-center" ${
+                            showDropdown ? "block" : "hidden"
+                            }`}
+                            style={{ position: "absolute" }}
+                        >
+                        <div className="dropDownItem border-b-2 border-secondaryBorder">
+                            <Link className="navLinkDropdown" 
+                                to="/tickets" 
+                                onClick={handleOptionClick}>
+                                Mis tickets
                                     </Link>
                                 </div>
                                 {userData.accessType === "producer" && (
