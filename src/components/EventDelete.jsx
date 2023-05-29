a62d3be4f378606fbb5da0d263313806119b9062
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-29 20:59:50 -0300

    event delete component

---
+++ src/components/EventDelete.jsx
@@ -0,0 +1,37 @@
+// Components
+import { useEffect, useState } from "react";
+import Tooltip from "./Tooltip";
+
+// Assets
+import { MdDeleteOutline } from "react-icons/md";
+
+const EventDelete = ({ tickets }) => {
+    const [canDelete, setCanDelete] = useState(true);
+
+    useEffect(() => {
+        const sells = tickets?.map((t) => t.sells);
+        console.log(sells);
+        const ticketsSolds = sells?.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+        );
+        console.log(ticketsSolds);
+        if (ticketsSolds) setCanDelete(false);
+    }, [tickets]);
+
+    return (
+        <Tooltip
+            tooltip={!canDelete ? "El ticket ya tiene ventas" : "Borrar ticket"}
+            x={!canDelete ? "150" : "70"}
+        >
+            <button
+                disabled={!canDelete}
+                className="disabled:cursor-not-allowed"
+            >
+                <MdDeleteOutline size="1.5rem" className="text-red-600" />
+            </button>
+        </Tooltip>
+    );
+};
+
+export default EventDelete;
