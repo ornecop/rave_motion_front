1fcdc589df2945e4ab71fbe6bdc4817345bd776a
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-30 23:42:17 -0300

    event container responsive

--- src/components/EventContainer.jsx
+++ src/components/EventContainer.jsx
@@ -1,23 +1,20 @@
-// import useSelector from 'react-redux'
 import EventCard from "./EventCard";
 
-const EventContainer = ({ events }) => {
+const EventContainer = ({ paginatedEvents }) => {
     return (
-        <div className="my-4 mx-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
-                {events.map((event) => (
-                    <EventCard
-                        key={event.id}
-                        id={event.id}
-                        name={event.name}
-                        producer={event.producer}
-                        image={event.image}
-                        hour={event.hour}
-                        venue={event.venue}
-                        date={event.date}
-                    />
-                ))}
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 m-4 min-h-screen justify-items-center">
+            {paginatedEvents.map((event) => (
+                <EventCard
+                    key={event.id}
+                    id={event.id}
+                    name={event.name}
+                    producer={event.producer}
+                    image={event.image}
+                    hour={event.hour}
+                    venue={event.venue}
+                    date={event.date}
+                />
+            ))}
         </div>
     );
 };
