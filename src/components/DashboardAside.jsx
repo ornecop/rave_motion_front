69be0f5ddd3d48b53d9b2a5bce0d1054a63a59f0
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-05-31 12:46:13 -0300

    dashboard aside responsive

--- src/components/DashboardAside.jsx
+++ src/components/DashboardAside.jsx
@@ -1,5 +1,8 @@
 // React Router Dom
-import { Link, redirect, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
 
 // Redux
 import { connect } from "react-redux";
@@ -14,9 +17,16 @@ import { GoLock } from "react-icons/go";
 import {
     MdOutlineDashboardCustomize,
     MdInsertChartOutlined,
+    MdArrowBackIos,
 } from "react-icons/md";
 
-const DashboardAside = ({ userData, isLogin, signOut }) => {
+const DashboardAside = ({
+    userData,
+    isLogin,
+    signOut,
+    showAside,
+    setShowAside,
+}) => {
     // SignOut
 
     const navigate = useNavigate();
@@ -26,85 +36,183 @@ const DashboardAside = ({ userData, isLogin, signOut }) => {
     };
 
     return (
-        <aside className="w-1/6 bg-secondary py-4">
-            {/* Section logo */}
-            <div className="flex w-full px-4 py-2 items-center h-16 gap-2">
-                <div className="bg-slate-500 rounded-full w-12 h-12 flex justify-center items-center text-2xl font-semibold">
-                    <span>
-                        {userData?.firstName &&
-                            userData.firstName[0].toUpperCase()}
-                    </span>
+        <>
+            {/* Aside */}
+            <aside className="hidden lg:block w-1/6 bg-secondary py-4">
+                {/* Section logo */}
+                <div className="flex w-full px-4 py-2 items-center h-16 gap-2">
+                    <div className="bg-slate-500 rounded-full w-12 h-12 flex justify-center items-center text-2xl font-semibold">
+                        <span>
+                            {userData?.firstName &&
+                                userData.firstName[0].toUpperCase()}
+                        </span>
+                    </div>
+                    <div className="flex justify-center items-center text-2xl font-semibold">
+                        <span>{userData.firstName}</span>
+                    </div>
                 </div>
-                <div className="flex justify-center items-center text-2xl font-semibold">
-                    <span>Ravemotion</span>
+
+                {/* Section App */}
+                <div className="dropDownItem mt-8">
+                    <Link className="navLinkDropdown" to="/dashboard">
+                        <div className="flex flex-row items-center gap-2">
+                            <MdOutlineDashboardCustomize size="1.5rem" />
+                            Dashboard
+                        </div>
+                    </Link>
                 </div>
-            </div>
+                <div className="dropDownItem ">
+                    <Link className="navLinkDropdown" to="/">
+                        <div className="flex flex-row items-center gap-2">
+                            <TiHomeOutline size="1.5rem" />
+                            Home
+                        </div>
+                    </Link>
+                </div>
 
-            {/* Section App */}
-            <div className="dropDownItem mt-8">
-                <Link className="navLinkDropdown" to="/dashboard">
-                    <div className="flex flex-row items-center gap-2">
-                        <MdOutlineDashboardCustomize size="1.5rem" />
-                        Dashboard
-                    </div>
-                </Link>
-            </div>
-            <div className="dropDownItem ">
-                <Link className="navLinkDropdown" to="/">
-                    <div className="flex flex-row items-center gap-2">
-                        <TiHomeOutline size="1.5rem" />
-                        Home
-                    </div>
-                </Link>
-            </div>
+                {/* Section Producer */}
+                <div className="dropDownItem mt-8">
+                    <Link className="navLinkDropdown">
+                        <div className="flex flex-row items-center gap-2">
+                            <MdInsertChartOutlined size="1.5rem" />
+                            Ventas
+                        </div>
+                    </Link>
+                </div>
+                <div className="dropDownItem ">
+                    <Link className="navLinkDropdown" to="/create">
+                        <div className="flex flex-row items-center gap-2">
+                            <BsCalendarPlus size="1.5rem" />
+                            Nuevo evento
+                        </div>
+                    </Link>
+                </div>
 
-            {/* Section Producer */}
-            <div className="dropDownItem mt-8">
-                <Link className="navLinkDropdown">
-                    <div className="flex flex-row items-center gap-2">
-                        <MdInsertChartOutlined size="1.5rem" />
-                        Ventas
+                {/* Section User */}
+                <div className="dropDownItem mt-8">
+                    <Link className="navLinkDropdown" to="/tickets">
+                        <div className="flex flex-row items-center gap-2">
+                            <IoTicketOutline size="1.5rem" />
+                            Tus tickets
+                        </div>
+                    </Link>
+                </div>
+                <div className="dropDownItem ">
+                    <Link className="navLinkDropdown" to="/changepassword">
+                        <div className="flex flex-row items-center gap-2">
+                            <GoLock size="1.5rem" />
+                            Cambiar contraseña
+                        </div>
+                    </Link>
+                </div>
+                <div className="dropDownItem ">
+                    <div className="navLinkDropdown">
+                        <div
+                            className="flex flex-row items-center gap-2"
+                            onClick={handleSignOutClick}
+                        >
+                            <VscSignOut size="1.5rem" />
+                            Cerrar sesión
+                        </div>
                     </div>
-                </Link>
-            </div>
-            <div className="dropDownItem ">
-                <Link className="navLinkDropdown" to="/create">
-                    <div className="flex flex-row items-center gap-2">
-                        <BsCalendarPlus size="1.5rem" />
-                        Nuevo evento
-                    </div>
-                </Link>
-            </div>
+                </div>
+            </aside>
 
-            {/* Section User */}
-            <div className="dropDownItem mt-8">
-                <Link className="navLinkDropdown" to="/tickets">
-                    <div className="flex flex-row items-center gap-2">
-                        <IoTicketOutline size="1.5rem" />
-                        Tus tickets
-                    </div>
-                </Link>
-            </div>
-            <div className="dropDownItem ">
-                <Link className="navLinkDropdown" to="/changepassword">
-                    <div className="flex flex-row items-center gap-2">
-                        <GoLock size="1.5rem" />
-                        Cambiar contraseña
-                    </div>
-                </Link>
-            </div>
-            <div className="dropDownItem ">
-                <div className="navLinkDropdown">
-                    <div
-                        className="flex flex-row items-center gap-2"
-                        onClick={handleSignOutClick}
+            {/* Aside responsive */}
+            {showAside && (
+                <aside className="lg:hidden fixed inset-0 z-50 flex bg-secondary bg-opacity-95">
+                    <button
+                        className="absolute left-4 top-4 flex flex-row btnPrimary items-center px-4 py-1 w-fit"
+                        onClick={() => setShowAside(false)}
                     >
-                        <VscSignOut size="1.5rem" />
-                        Cerrar sesión
+                        <MdArrowBackIos size="1.3rem" />
+                        Volver
+                    </button>
+                    <div className="w-full flex flex-col mt-16 px-4 py-8">
+                        {/* Section logo */}
+                        <div className="flex w-full px-4 py-2 items-center h-16 gap-2">
+                            <div className="bg-slate-500 rounded-full w-12 h-12 flex justify-center items-center text-2xl font-semibold">
+                                <span>
+                                    {userData?.firstName &&
+                                        userData.firstName[0].toUpperCase()}
+                                </span>
+                            </div>
+                            <div className="flex justify-center items-center text-2xl font-semibold">
+                                <span>{userData.firstName}</span>
+                            </div>
+                        </div>
+
+                        {/* Section App */}
+                        <div className="dropDownItem mt-8">
+                            <Link className="navLinkDropdown" to="/dashboard">
+                                <div className="flex flex-row items-center gap-2">
+                                    <MdOutlineDashboardCustomize size="1.5rem" />
+                                    Dashboard
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="dropDownItem ">
+                            <Link className="navLinkDropdown" to="/">
+                                <div className="flex flex-row items-center gap-2">
+                                    <TiHomeOutline size="1.5rem" />
+                                    Home
+                                </div>
+                            </Link>
+                        </div>
+
+                        {/* Section Producer */}
+                        <div className="dropDownItem mt-8">
+                            <Link className="navLinkDropdown">
+                                <div className="flex flex-row items-center gap-2">
+                                    <MdInsertChartOutlined size="1.5rem" />
+                                    Ventas
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="dropDownItem ">
+                            <Link className="navLinkDropdown" to="/create">
+                                <div className="flex flex-row items-center gap-2">
+                                    <BsCalendarPlus size="1.5rem" />
+                                    Nuevo evento
+                                </div>
+                            </Link>
+                        </div>
+
+                        {/* Section User */}
+                        <div className="dropDownItem mt-8">
+                            <Link className="navLinkDropdown" to="/tickets">
+                                <div className="flex flex-row items-center gap-2">
+                                    <IoTicketOutline size="1.5rem" />
+                                    Tus tickets
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="dropDownItem ">
+                            <Link
+                                className="navLinkDropdown"
+                                to="/changepassword"
+                            >
+                                <div className="flex flex-row items-center gap-2">
+                                    <GoLock size="1.5rem" />
+                                    Cambiar contraseña
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="dropDownItem ">
+                            <div className="navLinkDropdown">
+                                <div
+                                    className="flex flex-row items-center gap-2"
+                                    onClick={handleSignOutClick}
+                                >
+                                    <VscSignOut size="1.5rem" />
+                                    Cerrar sesión
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </aside>
+                </aside>
+            )}
+        </>
     );
 };
 
