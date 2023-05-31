30a6b4ec1d827de655171b420f0f6dd9e67a1e08
Facutorress <86375929+Facutorress@users.noreply.github.com>
2023-05-31 11:01:42 -0300

    lol

--- src/components/EventCardFinalized.jsx
+++ src/components/EventCardFinalized.jsx
@@ -101,6 +101,7 @@ const handleRateClick = () => {
         FINALIZADO
     </h2>
     <p>{averageRating}</p>
+    <p>{totalCritics}</p>
 </div>
 <div>
       <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded w-full text-center" onClick={handleRateClick}>
