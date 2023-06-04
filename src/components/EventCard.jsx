84efac2f5ecb1d62d6ded1351c14d909f22d35b8
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-06-03 21:57:52 -0300

    event carts mobile

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -8,7 +8,7 @@ export const EventCard = ({ id, name, image, date, venue, hour }) => {
     const formatDate = date.slice(0, 10).split("-").reverse().join("-");
 
     return (
-        <div className="h-[21rem] w-48 bg-secondary rounded-xl border border-secondaryBorder">
+        <div className="h-[21.5rem] w-48 bg-secondary rounded-xl border border-secondaryBorder">
             <div className="w-full aspect-square rounded-t-xl">
                 <Link to={`/event/${id}`}>
                     <img
@@ -20,7 +20,7 @@ export const EventCard = ({ id, name, image, date, venue, hour }) => {
                 </Link>
             </div>
             <div className="mx-2 my-2">
-                <div className="flex flex-row h-14 pb-2 border-b border-secondaryBorder">
+                <div className="flex flex-row h-16 pb-2 border-b border-secondaryBorder">
                     <h2 className="text-sm font-semibold">{name}</h2>
                 </div>
                 <div className="flex flex-row p-2 gap-2 text-sm mb-2 border-b border-secondaryBorder">
