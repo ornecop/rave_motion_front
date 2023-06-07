ed0ab992f7510aff3c0832c5f02216cd22bf2d6e
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-06-07 12:44:01 -0300

    dashboard aside

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -3,14 +3,26 @@ import useDarkMode from "../hooks/useDarkMode";
 // Icons
 import { MdOutlineDarkMode, MdOutlineWbSunny } from "react-icons/md";
 
-const DarkModeSwitch = () => {
+const DarkModeSwitch = ({ blockType }) => {
     const [theme, setTheme] = useDarkMode();
 
     const handleChange = () => {
         setTheme((prev) => (prev === "dark" ? "light" : "dark"));
     };
 
-    return (
+    return blockType ? (
+        <button
+            onClick={handleChange}
+            className="w-full gap-2 items-center flex flex-row text-lg"
+        >
+            {theme === "dark" ? (
+                <MdOutlineDarkMode className="text-text" size="1.5rem" />
+            ) : (
+                <MdOutlineWbSunny className="text-textLight" size="1.5rem" />
+            )}
+            Cambiar aspecto
+        </button>
+    ) : (
         <button onClick={handleChange}>
             {theme === "dark" ? (
                 <MdOutlineDarkMode className="text-fuchsia-600" size="1.8rem" />
